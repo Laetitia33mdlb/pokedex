b32b155250597572f3e82df215139afaa5c23f98
Laetitia <laetitia.mdlb@gmail.com>
2023-03-28 21:23:40 +0200

    add prop-types

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,17 +1,25 @@
+import PropTypes from "prop-types";
 
-function PokemonCard(props) {
-    
-    return (
-      <div>
-        <figure>
-                {props.pokemon.imgSrc !== undefined 
-                    ? <img src={props.pokemon.imgSrc} alt="image pokemon"/>
-                    : <p>???</p>
-                }    
-            
-            <figcaption>{props.pokemon.name}</figcaption>
-        </figure>
-      </div>
-    );
-  }
-  export default PokemonCard;
+function PokemonCard({pokemon}) {
+  return (
+    <div>
+      <figure>
+        {pokemon.imgSrc !== undefined ? (
+          <img src={pokemon.imgSrc} alt="image pokemon" />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+}
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
+};
+
+export default PokemonCard;
