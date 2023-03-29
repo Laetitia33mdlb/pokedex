e6f64ee100510a3395c3b5170eb44ec7b91cdc1e
Laetitia <laetitia.mdlb@gmail.com>
2023-03-29 22:32:36 +0200

    create state.1

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -11,36 +11,50 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
+
 function App() {
-  const [count, setCount] = useState(0);
-  const pokemon = pokemonList[0];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const pokemon = pokemonList[pokemonIndex];
+
+  const handlePrevious = (event) => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
+  const handleNext = (event) => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
 
   return (
     <div className="App">
       <PokemonCard pokemon={pokemon} />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {pokemonIndex > 0 ? (
+        <button onClick={handlePrevious}>Previous</button>
+      ) : (
+        <span></span>
+      )}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={handleNext}>Next</button>
+      ) : (
+        <span></span>
+      )}
     </div>
   );
 }
